14d5f623a77f26301146f3f0080b1a9d3f04e494
Shivindera Singh <shivindera@gmail.com>
2024-10-13 00:14:18 +0200

    Update __init__.py
    Add change to store file locally

--- custom_components/recorder_service/__init__.py
+++ custom_components/recorder_service/__init__.py
@@ -10,11 +10,12 @@ def setup(hass, config):
         wav = call.data.get(WAV_ATTR_NAME, '')
         target = call.data.get(URL_ATTR_NAME, '')
 
-        # send wav to server
-        url = target
-        payload = bytearray(wav)
-        headers = {'content-type': 'audio/wav', 'Accept': 'application/json'}
-        request = requests.post(url, data=payload, headers=headers)
+        # Convert the list of integers to bytes
+        wav_bytes = bytes(wav)
+
+        # Write the bytes to a file
+        with open(target, "wb") as f:
+            f.write(wav_bytes)
 
     hass.services.register(DOMAIN, "process", handle_recorder)
 
