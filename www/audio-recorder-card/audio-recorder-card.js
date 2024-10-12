e9bff0449b4c93d6fd43de9bdbba5455e6e68dc6
Shivindera Singh <shivindera@gmail.com>
2024-10-13 00:26:34 +0200

    Update audio-recorder-card.js
    Update file to better handle Recorder.js library loading

--- www/audio-recorder-card/audio-recorder-card.js
+++ www/audio-recorder-card/audio-recorder-card.js
@@ -1,116 +1,95 @@
-function loadScript(type, url)
-{
-  if(type.toUpperCase() == "CSS")
-  {
-    const link = document.createElement("link");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = url;
-    document.head.appendChild(link);
-  }
-  else if (type.toUpperCase() == "JS")
-  {
-    const link = document.createElement("script");
-    link.src = url;
-    document.head.appendChild(link);
-  }
+function loadRecorderJS(callback) {
+  const script = document.createElement('script');
+  script.src = 'https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js';
+  script.onload = callback;
+  document.head.appendChild(script);
 }
 
-function loadRecorderJS()
-{
-  loadScript("JS", "https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js");
+function isNull(test, value) {
+  if (test != undefined && test != null) value = test;
+
+  return value;
 }
 
-function isNull(test, value)
-{
-    if(test != undefined && test != null)
-        value = test;
-
-    return value;
-}
-
-function toggleRecording(button, hass, config)
-{
-    if(config.cardRec != null && config.cardRec.recording)
-        stopRecording(button, hass, config);
-    else
+function toggleRecording(button, hass, config) {
+  if (config.cardRec != null && config.cardRec.recording) {
+    stopRecording(button, hass, config);
+  } else {
+    if (typeof Recorder === 'undefined') {
+      loadRecorderJS(() => {
         startRecording(button, hass, config);
+      });
+    } else {
+      startRecording(button, hass, config);
+    }
+  }
 }
 
-function startRecording(button, hass, config)
-{
-    try
-    {
-        var constraints = { audio: true, video: false };
+function startRecording(button, hass, config) {
+  try {
+    var constraints = { audio: true, video: false };
 
-	if(typeof Recorder == "undefined")
-            loadRecorderJS();
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
 
-        var AudioContext = window.AudioContext || window.webkitAudioContext;
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then(function (stream) {
+        var audioContext = new AudioContext();
+        config.gumStream = stream;
+        var cardInput = audioContext.createMediaStreamSource(stream);
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-	    var audioContext = new AudioContext();
-	    config.gumStream = stream;
-	    var cardInput = audioContext.createMediaStreamSource(stream);
+        config.cardRec = new Recorder(cardInput, { numChannels: 1 });
+        config.cardRec.record();
 
-	    config.cardRec = new Recorder(cardInput,{ numChannels: 1 });
-	    config.cardRec.record();
+        var icon = button.getElementsByTagName('img')[0];
+        icon.src = icon.getAttribute('data-icon-rec');
 
-            var icon = button.getElementsByTagName("img")[0];
-            icon.src = icon.getAttribute('data-icon-rec');
-
-	    button.getElementsByClassName("isRecording")[0].classList.remove('hide');
-	}).catch(function(err) {
-	    showMessage(err.toString(), err.message, config);
-	});
-    }
-    catch(err)
-    {
-	showMessage("Error", "Unknown - " + err.message, config);
-    }
+        button.getElementsByClassName('isRecording')[0].classList.remove('hide');
+      })
+      .catch(function (err) {
+        showMessage(err.toString(), err.message, config);
+      });
+  } catch (err) {
+    showMessage('Error', 'Unknown - ' + err.message, config);
+  }
 }
 
-function stopRecording(button, hass, config)
-{
-    config.cardRec.stop();
-    config.gumStream.getAudioTracks()[0].stop();
+function stopRecording(button, hass, config) {
+  config.cardRec.stop();
+  config.gumStream.getAudioTracks()[0].stop();
 
-    var icon = button.getElementsByTagName("img")[0];
-    icon.src = icon.getAttribute('data-icon-img');
+  var icon = button.getElementsByTagName('img')[0];
+  icon.src = icon.getAttribute('data-icon-img');
 
-    button.getElementsByClassName("isRecording")[0].classList.add('hide');
+  button.getElementsByClassName('isRecording')[0].classList.add('hide');
 
-    config.cardRec.exportWAV(function(blob) {
-        blob.arrayBuffer().then(buffer => {
-             hass.callService("recorder_service", "process", {
-                 wav: Array.from(new Uint8Array(buffer)),
-                 target: config.TARGET
-             });
-         });
+  config.cardRec.exportWAV(function (blob) {
+    blob.arrayBuffer().then((buffer) => {
+      hass.callService('recorder_service', 'process', {
+        wav: Array.from(new Uint8Array(buffer)),
+        target: config.TARGET,
+      });
     });
+  });
 }
 
-function showMessage(type, message, config)
-{
-    config.info.classList.remove("hide");
-    config.info.innerHTML = type + ": " + message;
+function showMessage(type, message, config) {
+  config.info.classList.remove('hide');
+  config.info.innerHTML = type + ': ' + message;
 
-    if(config.timeout != null)
-        clearTimeout(config.timeout);
+  if (config.timeout != null) clearTimeout(config.timeout);
 
-    config.timeout = window.setTimeout(function() {
-        hideMessage(config);
-    }, 5000);
+  config.timeout = window.setTimeout(function () {
+    hideMessage(config);
+  }, 5000);
 }
 
-function hideMessage(config)
-{
-    config.info.classList.add("hide");
-    config.info.innerHTML = "";
+function hideMessage(config) {
+  config.info.classList.add('hide');
+  config.info.innerHTML = '';
 
-    config.timeout = null;
+  config.timeout = null;
 }
-
 
 class AudioRecorderCard extends HTMLElement {
   set hass(hass) {
@@ -128,21 +107,20 @@ class AudioRecorderCard extends HTMLElement {
       var card = document.createElement('ha-card');
 
       // header if exists
-      const header = isNull(this.config.header, '')
+      const header = isNull(this.config.header, '');
 
-      if(header.length > 0)
-          card.header = header;
+      if (header.length > 0) card.header = header;
 
       // record button
       this.content = document.createElement('div');
-      this.content.id = "container";
+      this.content.id = 'container';
 
       const params = this.vars;
       var recorder = document.createElement('button');
-      recorder.onclick = function() {
+      recorder.onclick = function () {
         toggleRecording(this, hass, params);
       };
-      recorder.id = "buttonRecord";
+      recorder.id = 'buttonRecord';
 
       var iconImg = isNull(this.config.icon, '/local/audio-recorder-card/images/microphone.svg');
       var iconRec = isNull(this.config.icon_rec, '/local/audio-recorder-card/images/microphone_rec.svg');
@@ -252,16 +230,15 @@ class AudioRecorderCard extends HTMLElement {
 }
     `;
 
-     style = style.split('$size').join(isNull(size, '50') + 'px');
-     style = style.split('$color').join( isNull(color, '#03A9F4'));
-     style = style.split('$image_size').join((parseInt(isNull(size, '50')) / 2) + 'px');
+    style = style.split('$size').join(isNull(size, '50') + 'px');
+    style = style.split('$color').join(isNull(color, '#03A9F4'));
+    style = style.split('$image_size').join(parseInt(isNull(size, '50')) / 2 + 'px');
 
-     this.styles.innerHTML = style;
+    this.styles.innerHTML = style;
   }
 
   setConfig(config) {
-    if(!config.target)
-        throw new Error('Please define a target url for your record');
+    if (!config.target) throw new Error('Please define a target url for your record');
 
     this.config = config;
   }
@@ -270,8 +247,8 @@ class AudioRecorderCard extends HTMLElement {
 customElements.define('audio-recorder-card', AudioRecorderCard);
 window.customCards = window.customCards || [];
 window.customCards.push({
-  type: "audio-recorder-card",
-  name: "Audio Recorder Card",
+  type: 'audio-recorder-card',
+  name: 'Audio Recorder Card',
   preview: true,
-  description: "Audio-Recorder to send your voice to a server."
+  description: 'Audio-Recorder to send your voice to a server.',
 });
